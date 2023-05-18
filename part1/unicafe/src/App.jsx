3f04cd8c7bc88c9459fbaf2f1711aa1b7ac2b964
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-19 04:21:33 +0800

    done 1.6-1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,23 @@ function App() {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	return <div>code here</div>;
+	return (
+		<div>
+			<h2>give feedback</h2>
+
+			<button onClick={() => setGood(good + 1)}>good</button>
+			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+			<button onClick={() => setBad(bad + 1)}>bad</button>
+
+			<h2>statistics</h2>
+			<p>Good: {good}</p>
+			<p>Neutral: {neutral}</p>
+			<p>Bad: {bad}</p>
+			<p>All: {good + neutral + bad}</p>
+			<p>Average: {(good - bad) / (good + neutral + bad)}</p>
+			<p>Positive: {(good / (good + neutral + bad)) * 100}%</p>
+		</div>
+	);
 }
 
 export default App;
