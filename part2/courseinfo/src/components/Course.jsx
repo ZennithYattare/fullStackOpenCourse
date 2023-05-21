7afc43ebe80cf15f2267d09fa6ab6589cb07ee21
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-21 21:50:27 +0800

    done 2.2-2.3: Course information step7-8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,11 +5,16 @@ const Header = ({ course }) => {
 };
 
 const Content = ({ parts }) => {
+	const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
 	return (
 		<div>
 			{parts.map((part) => (
 				<Part key={part.id} part={part} />
 			))}
+			<p>
+				<strong>total of {totalExercises} exercises</strong>
+			</p>
 		</div>
 	);
 };
