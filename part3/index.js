940b300c1c156240e1a0c9cebe4539707c804c73
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-25 23:32:21 +0800

    done 3.2: Phonebook backend step2

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,12 @@ app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+	const date = new Date();
+	const message = `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`;
+	res.send(message);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
