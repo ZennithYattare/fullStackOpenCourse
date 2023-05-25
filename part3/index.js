42625b7c8d4a4442b875c7c0c03a6b7c3ffe7d6c
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-26 00:00:25 +0800

    done 3.6: Phonebook backend step6

--- part3/index.js
+++ part3/index.js
@@ -64,6 +64,18 @@ app.delete("/api/persons/:id", (req, res) => {
 app.post("/api/persons", (req, res) => {
 	const body = req.body;
 
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: "content missing",
+		});
+	}
+
+	if (persons.find((person) => person.name === body.name)) {
+		return res.status(400).json({
+			error: "name must be unique",
+		});
+	}
+
 	const person = {
 		id: Math.floor(Math.random() * 100000),
 		name: body.name,
