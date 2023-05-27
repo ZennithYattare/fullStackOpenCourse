b0dc841a2d71658bb8d4477c649886a5e510bfd7
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-28 04:06:47 +0800

    done 3.20*: Phonebook database, step8

--- part3/models/person.js
+++ part3/models/person.js
@@ -25,7 +25,17 @@ const personSchema = new mongoose.Schema({
 		minLength: 3,
 		required: true,
 	},
-	number: String,
+	number: {
+		type: String,
+		required: true,
+		validate: {
+			validator: function (v) {
+				return /^\d{2,3}-\d{6,}$/.test(v);
+			},
+			message: (props) =>
+				`${props.value} is not a valid phone number. Please use the format XX-XXXXXXX or XXX-XXXXXXX.`,
+		},
+	},
 });
 
 personSchema.set("toJSON", {
