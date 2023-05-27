5dcaeafa69f233e5e05e47a855ada7bc67cb7e5d
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-28 03:43:38 +0800

    return to SPA baseUrl

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -9,20 +9,18 @@
 // I think it was undefined because the error was caught in persons.jsx, so it didn't have a chance to be caught in App.jsx causing it to be undefined.
 
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
 	const request = axios.get(baseUrl);
-	return request
-		.then((response) => response.data)
-		// .catch((error) => console.log(error.toJSON()));
+	return request.then((response) => response.data);
+	// .catch((error) => console.log(error.toJSON()));
 };
 
 const create = (newObject) => {
 	const request = axios.post(baseUrl, newObject);
-	return request
-		.then((response) => response.data)
-		// .catch((error) => console.log(error.toJSON()));
+	return request.then((response) => response.data);
+	// .catch((error) => console.log(error.toJSON()));
 };
 
 const update = (id, newObject) => {
