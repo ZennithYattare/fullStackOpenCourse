c6fd998eeea61bede54d883a03b229bfd85acba8
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-27 15:21:01 +0800

    mongodb model for database interaction

---
+++ part3/models/person.js
@@ -0,0 +1,35 @@
+/** @format */
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: "phonebook",
+	})
+	.then(() => {
+		console.log("Connected to MongoDB.");
+	})
+	.catch((err) => console.log("error connecting to MongoDB:", err.message));
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+});
+
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+module.exports = mongoose.model("Person", personSchema);
