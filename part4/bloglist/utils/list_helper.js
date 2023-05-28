9391b72d330f5ecfca69f39789391f2c6a7810e1
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-28 23:42:31 +0800

    done 4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,6 +5,13 @@ const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = (blogs) => {
+	const likes = blogs.map((blog) => blog.likes);
+	const sum = likes.reduce((acc, curr) => acc + curr, 0);
+	return sum;
+};
+
 module.exports = {
 	dummy,
+	totalLikes,
 };
