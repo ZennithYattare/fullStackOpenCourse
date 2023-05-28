1712878917a9d1258739bd8711428bf5a1081227
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-28 22:32:42 +0800

    done 4.2 Blog list, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,44 @@
+/** @format */
+
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+	.connect(config.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: "bloglist",
+	})
+	.then(() => {
+		console.log("Connected to MongoDB.");
+	})
+	.catch((err) => console.log("Error connecting to MongoDB:", err.message));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+	morgan(
+		":method :url :status :res[content-length] - :response-time ms :body"
+	)
+);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
