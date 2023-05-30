9258f938b177f66c4e853a3bf63774353933aa60
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-30 13:51:37 +0800

    done 4.13-14 Blog list expansions, step1-2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -20,8 +20,8 @@ beforeEach(async () => {
 	}
 });
 
-describe("blogs", () => {
-	test("are returned as JSON", async () => {
+describe("when there is initially some notes saved", () => {
+	test("blogs are returned as JSON", async () => {
 		await api
 			.get("/api/blogs")
 			.expect(200)
@@ -37,7 +37,9 @@ describe("blogs", () => {
 		const response = await api.get("/api/blogs");
 		expect(response.body[0].id).toBeDefined();
 	});
+});
 
+describe("addition of a new blog", () => {
 	test("a valid blog can be added", async () => {
 		const newBlog = {
 			title: "New blog post",
@@ -95,6 +97,47 @@ describe("blogs", () => {
 
 		const blogsAtEnd = await helper.blogsInDb();
 		expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+	});
+});
+
+describe("upating of a blog", () => {
+	test("a blog post can be updated", async () => {
+		const blogsAtStart = await helper.blogsInDb();
+		const blogToUpdate = blogsAtStart[0];
+
+		const updatedBlog = {
+			...blogToUpdate,
+			likes: blogToUpdate.likes + 1,
+		};
+
+		await api
+			.put(`/api/blogs/${blogToUpdate.id}`)
+			.send(updatedBlog)
+			.expect(200);
+
+		const blogsAtEnd = await helper.blogsInDb();
+		const updatedBlogInDb = blogsAtEnd.find(
+			(blog) => blog.id === blogToUpdate.id
+		);
+
+		expect(updatedBlogInDb.likes).toBe(blogToUpdate.likes + 1);
+	});
+});
+
+describe("deletion of a blog", () => {
+	test("a blog post can be deleted", async () => {
+		const blogsAtStart = await helper.blogsInDb();
+		const blogToDelete = blogsAtStart[0];
+
+		await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+		const blogsAtEnd = await helper.blogsInDb();
+
+		expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1);
+
+		const titles = blogsAtEnd.map((r) => r.title);
+
+		expect(titles).not.toContain(blogToDelete.title);
 	});
 });
 
