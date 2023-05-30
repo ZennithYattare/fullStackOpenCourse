ecd185248ef9e36836c80455f2b049352b6ac50d
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-30 12:36:06 +0800

    done 4.8: Blog list tests, step1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,39 @@
+/** @format */
+
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
+
+// const logger = require("../utils/logger");
+
+beforeEach(async () => {
+	await Blog.deleteMany({});
+
+	for (let blog of helper.initialBlogs) {
+		let blogObject = new Blog(blog);
+		await blogObject.save();
+	}
+});
+
+describe("blogs", () => {
+	test("are returned as JSON", async () => {
+		await api
+			.get("/api/blogs")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
+	});
+
+	test("returns the correct amount of blog posts", async () => {
+		const response = await api.get("/api/blogs");
+		expect(response.body).toHaveLength(helper.initialBlogs.length);
+	});
+});
+
+afterAll(async () => {
+	await mongoose.connection.close();
+});
