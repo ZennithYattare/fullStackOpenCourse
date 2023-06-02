84fc1aad74e27732a8da207c429a593cc8846cbf
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-03 00:40:35 +0800

    done 5.7-8 Blog list frontend, step7-8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+/** @format */
 
-export default Blog
+import React, { useState } from "react";
+
+const Blog = ({ blog, user }) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const [showDetails, setShowDetails] = useState(false);
+
+	const toggleDetails = () => {
+		setShowDetails(!showDetails);
+	};
+
+	return (
+		<div style={blogStyle}>
+			<div>
+				{blog.title} {blog.author}{" "}
+				<button onClick={toggleDetails}>
+					{showDetails ? "hide" : "view"}
+				</button>
+			</div>
+			{showDetails && (
+				<div>
+					<a href={blog.url}>{blog.url}</a>
+					<p>
+						likes {blog.likes} <button>like</button>
+					</p>
+					<p>{blog.user.name}</p>
+					{user && user.username === blog.user.username && (
+						<button>delete</button>
+					)}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Blog;
