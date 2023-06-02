2ab3dd5ab197d98d075da8b903ea45bf9686d87a
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-02 20:59:08 +0800

    removed unused lines of code

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -14,25 +14,21 @@ const baseUrl = "/api/persons";
 const getAll = () => {
 	const request = axios.get(baseUrl);
 	return request.then((response) => response.data);
-	// .catch((error) => console.log(error.toJSON()));
 };
 
 const create = (newObject) => {
 	const request = axios.post(baseUrl, newObject);
 	return request.then((response) => response.data);
-	// .catch((error) => console.log(error.toJSON()));
 };
 
 const update = (id, newObject) => {
 	const request = axios.put(`${baseUrl}/${id}`, newObject);
 	return request.then((response) => response.data);
-	// .catch((error) => console.log(error.toJSON()));
 };
 
 const deletePerson = (id) => {
 	const request = axios.delete(`${baseUrl}/${id}`);
 	return request.then((response) => response.data);
-	// .catch(() => alert("Data to be deleted does not exist."));
 };
 
 export default {
