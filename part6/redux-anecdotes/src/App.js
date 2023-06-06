9bc4aad8f927c305c1c3481d3fbbdca2c30a3a45
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-07 02:03:38 +0800

    done 6.14 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import anecdoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		anecdoteService.getAll().then((anecdotes) => {
+			dispatch(setAnecdotes(anecdotes));
+		});
+	}, [dispatch]);
+
 	return (
 		<div>
 			<h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,29 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-	"If it hurts, do it more often",
-	"Adding manpower to a late software project makes it later!",
-	"The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-	"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-	"Premature optimization is the root of all evil.",
-	"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-	return {
-		content: anecdote,
-		id: getId(),
-		votes: 0,
-	};
-};
-
-const initialState = anecdotesAtStart.map(asObject);
 
 const anecdoteSlice = createSlice({
 	name: "anecdotes",
-	initialState,
+	initialState: [],
 	reducers: {
 		addAnecdote: (state, action) => {
 			console.log("state", state);
@@ -43,9 +24,20 @@ const anecdoteSlice = createSlice({
 			anecdoteToVote.votes++;
 			state.sort((a, b) => b.votes - a.votes);
 		},
+		appendAnecdotes: (state, action) => {
+			console.log("state", state);
+			console.log("action", action);
+			state.push(action.payload);
+		},
+		setAnecdotes: (state, action) => {
+			console.log("state", state);
+			console.log("action", action);
+			return action.payload;
+		},
 	},
 });
 
-export const { addAnecdote, incrementVote } = anecdoteSlice.actions;
+export const { addAnecdote, incrementVote, appendAnecdotes, setAnecdotes } =
+	anecdoteSlice.actions;
 
 export default anecdoteSlice.reducer;
