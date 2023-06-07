dd3c1874af5ce1daf0763ff6c74263a284225455
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-08 02:46:51 +0800

    done 7.5: anecdotes and hooks step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -106,6 +106,12 @@ const CreateNew = (props) => {
 		});
 	};
 
+	const handleReset = () => {
+		content.reset();
+		author.reset();
+		info.reset();
+	};
+
 	return (
 		<div>
 			<h2>create a new anecdote</h2>
@@ -123,6 +129,9 @@ const CreateNew = (props) => {
 					<input {...info} />
 				</div>
 				<button>create</button>
+				<button type="button" onClick={handleReset}>
+					reset
+				</button>
 			</form>
 		</div>
 	);
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -7,9 +7,15 @@ export const useField = (type) => {
 		setValue(event.target.value);
 	};
 
+	// a reset function is added to the hook
+	const reset = () => {
+		setValue("");
+	};
+
 	return {
 		type,
 		value,
 		onChange,
+		reset,
 	};
 };
