fb113e88bdd649263d6dfc9c79e426c9bb13b835
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-07 17:35:09 +0800

    done Exercise 6.20

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,40 +1,57 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery } from "react-query";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
+	const handleVote = (anecdote) => {
+		console.log("vote");
+	};
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+	const {
+		data: anecdotes,
+		isLoading,
+		isError,
+		error,
+	} = useQuery("anecdotes", getAnecdotes, {
+		retry: 3,
+	});
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
+	if (isError) {
+		return (
+			<p>
+				Anecdote service not available due to problems in server.{" "}
+				<p>Error: {error.message}</p>
+			</p>
+		);
+	}
 
-export default App
+	console.log("anecdotes:", anecdotes);
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>
+							vote
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
