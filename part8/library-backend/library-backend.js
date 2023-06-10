1bc1ff1000ab9367f106e28588a2917a4096a81c
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-10 09:07:56 +0800

    done 8.2: All books

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -99,9 +99,17 @@ let books = [
 */
 
 const typeDefs = gql`
+	type Book {
+		title: String!
+		author: String!
+		published: Int!
+		genres: [String!]!
+	}
+
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
+		allBooks: [Book!]!
 	}
 `;
 
@@ -109,6 +117,7 @@ const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
+		allBooks: () => books,
 	},
 };
 
