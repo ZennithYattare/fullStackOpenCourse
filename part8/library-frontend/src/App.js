c9fbff22c575d2928169cd9b5786954d07412b1f
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-12 01:58:56 +0800

    use React router

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,26 +1,24 @@
-import { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import { Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+	return (
+		<div>
+			<div>
+				<Link to="/">authors</Link>
+				<Link to="/books">books</Link>
+				<Link to="/add">add book</Link>
+			</div>
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+			<Routes>
+				<Route path="/" element={<Authors />} />
+				<Route path="/books" element={<Books />} />
+				<Route path="/add" element={<NewBook />} />
+			</Routes>
+		</div>
+	);
+};
 
-      <Authors show={page === 'authors'} />
-
-      <Books show={page === 'books'} />
-
-      <NewBook show={page === 'add'} />
-    </div>
-  )
-}
-
-export default App
+export default App;
