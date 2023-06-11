c1aab727f026f0dd123ccb34c91404b39af9bebc
ZennithYattare <luis.espera.0758@gmail.com>
2023-06-12 02:33:07 +0800

    done 8.10: Adding a book

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { GET_BOOKS, GET_AUTHORS, ADD_BOOK } from "../queries";
 
 const NewBook = () => {
 	const [title, setTitle] = useState("");
@@ -6,11 +8,31 @@ const NewBook = () => {
 	const [published, setPublished] = useState("");
 	const [genre, setGenre] = useState("");
 	const [genres, setGenres] = useState([]);
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	const [addBook] = useMutation(ADD_BOOK, {
+		refetchQueries: [{ query: GET_BOOKS }, { query: GET_AUTHORS }],
+		onError: (error) => {
+			setErrorMessage(error.graphQLErrors[0].message);
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		},
+	});
 
 	const submit = async (event) => {
 		event.preventDefault();
 
 		console.log("add book...");
+
+		addBook({
+			variables: {
+				title,
+				author,
+				published: parseInt(published),
+				genres,
+			},
+		});
 
 		setTitle("");
 		setPublished("");
@@ -26,6 +48,7 @@ const NewBook = () => {
 
 	return (
 		<div>
+			{errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
 			<form onSubmit={submit}>
 				<div>
 					title
